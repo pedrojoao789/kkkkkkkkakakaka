9aee1edc674931d518105cd05c52eb7c8eeaa29b
pedrojoao789 <104779045+pedrojoao789@users.noreply.github.com>
2023-10-31 12:06:59 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form")
+const resp = document.querySelector("pre")
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    const numero = Number.(frm.inllumero.valeu)
+    let resposta = "" 
+     
+    for(let i = i; i <= 10 i++){
+        resposta = resposta +_numero + " x " + i + " = "+ (numero * i) + "\n"
+    
+     }
+     resp,innerText = resposta 
+    })
